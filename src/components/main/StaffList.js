f4080b68f3db9bd95e0f1f7ea4c5ed703b5682b9
ThatOneBritishGuy <Jonathanmcleod@u.boisestate.edu>
2022-03-01 15:56:55 -0700

    Apparently fixes test bug

--- src/components/main/StaffList.js
+++ src/components/main/StaffList.js
@@ -78,14 +78,13 @@ class StaffList extends React.Component {
             <td >
                 <Trash className="bTrash" data-testid="delete-id" onClick={this.listRemove.bind(staff,i)} />
             </td>
-            
+            <td>{staff.staffType}</td>
             <td>
                <Plus className="bPlus" data-testid="add-id" onClick={this.listAdd.bind(staff,i)}/> 
                {staff.quantity} 
                <Dash className="bDash" data-testid="minus-id" onClick={this.listSub.bind(staff,i)}/>
             </td>
             <td>{staff.shift}</td>
-           
             <td>{staff.shiftTotal}</td>
         </tr>
 
