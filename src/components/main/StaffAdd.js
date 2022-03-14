b7e189ed41e9700d3bc26fe6fc4eb590a681a983
Andrew Gerber <andrewgerber@u.boisestate.edu>
2022-03-14 12:42:40 -0600

    impored

--- src/components/main/StaffAdd.js
+++ src/components/main/StaffAdd.js
@@ -3,6 +3,7 @@ import { Modal, Button, Form } from "react-bootstrap";
 import './StaffAdd.css';
 import logo from '../../assets/noun-help-2492040.png';
 import Tooltip from "./Tooltip";
+import EventCalendar from "./EventCalendar";
 
 // also noticing another bug where the url is holding the value of the selected items in the modal. We should look at how to fix this in the next sprint because it might look different after the refactor
 class StaffAdd extends React.Component {
