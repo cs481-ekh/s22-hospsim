911e369e00a6d93582508623f89966c61f3bd8de
Derek <derekvalenzuela@u.boisestate.edu>
2022-03-14 11:32:11 -0600

    Test progress on custom view
    test done with 3 day and altered button text

--- src/components/main/EventCalendar.js
+++ src/components/main/EventCalendar.js
@@ -2,6 +2,8 @@ import React,{Component} from 'react'
 import "./Scenario.css";
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction';
+import timeGridPlugin from '@fullcalendar/timegrid';
 
 export default class EventCalendar extends Component{
 
@@ -12,9 +14,19 @@ export default class EventCalendar extends Component{
                 <div>
 
                 <FullCalendar
-
-                defaultView="dayGridMonth"
-                plugins={[ dayGridPlugin ]}
+                plugins={[ dayGridPlugin, interactionPlugin , timeGridPlugin]}
+                views = {{
+                 agendaThreeDay: {
+                      type: 'agenda',
+                      duration: { days: 3 },
+                      buttonText: '3 day'
+                 }
+                }}
+                selectable
+                initialView = 'agendaThreeDay'
+                headerToolbar={{
+                    center: 'dayGridMonth,agendaThreeDay,timeGridDay new',
+                  }}
                 events={[
                     //event code here
                     { title: 'added staff here', date: '2022-03-10' }
@@ -27,5 +39,6 @@ export default class EventCalendar extends Component{
         )
 
     }
+    
 
 }
