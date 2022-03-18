d5ce98dc2d1e92d3b1b79396a6432eda4cd7290a
Andrew Gerber <andrewgerber@u.boisestate.edu>
2022-03-17 18:11:10 -0600

    finished colors and propulated calander for dev

--- src/components/main/Scenario.js
+++ src/components/main/Scenario.js
@@ -20,8 +20,9 @@ class Scenario extends React.Component {
       num: "",
       center: { "text-align": "center" },
       staffs: [],
-      textColor: ["Red","Green","Yellow"], //0 is RN, 1 is LVN, 2 is Unlic
-      backgroundColor: ["SlateGrey","DarkGrey","Lavender","DarkOrchid","DarkSlateBlue"], //0 is 12 day, 1 is 12 night, 2 is 8 day, 3 is 8 evening, 4 is 8 night
+      // textColor: ["Red","Green","Yellow"], //0 is RN, 1 is LVN, 2 is Unlic
+      textColor: ["black","black","white"], //0 is RN, 1 is LVN, 2 is Unlic
+      backgroundColor: ["Khaki","DarkGrey","Lavender","DarkOrchid","MidnightBlue"], //0 is 12 day, 1 is 12 night, 2 is 8 day, 3 is 8 evening, 4 is 8 night
       showBudget: false,
       showCal: true,  //just cuz
       calEvents: [],
